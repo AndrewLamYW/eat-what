39fd9cbafd49f77384c51a6d8346d250970dfc11
Andrew Lam <andrewlam.yw@gmail.com>
2024-04-25 21:24:16 +0800

    Update AuthenticatedHome component to include a form for creating a session

--- app/page-authenticated.tsx
+++ app/page-authenticated.tsx
@@ -1,5 +1,65 @@
 import { SignedIn } from "@clerk/nextjs";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Container from "@mui/material/Container";
+import Stack from "@mui/material/Stack";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
 
 export default function AuthenticatedHome() {
-  return <SignedIn>Plan a group meal</SignedIn>;
+  return (
+    <SignedIn>
+      <Container maxWidth="md">
+        <Card>
+          <CardContent sx={{ p: { sm: 4, md: 5, textAlign: "center" } }}>
+            <Typography variant="h2" gutterBottom>
+              <b>Let&apos;s eat together!</b>
+            </Typography>
+
+            <Typography variant="subtitle1" sx={{ mb: 2 }}>
+              Set your intention and invite your friends to join you! 🖐🏻 🖐🏼 🖐🏽
+              🖐🏾 🖐🏿
+            </Typography>
+
+            <Stack spacing={4}>
+              <TextField label="Session Title" variant="filled" />
+
+              <Box>
+                <Button variant="contained">CREATE SESSION</Button>
+              </Box>
+
+              {/* <FormControl>
+                <FormLabel id="demo-radio-buttons-group-label">Date</FormLabel>
+
+                <RadioGroup defaultValue="fix" name="radio-buttons-group">
+                  <FormControlLabel
+                    value="fix"
+                    control={<Radio />}
+                    label="Fix"
+                  />
+                  <FormControlLabel
+                    value="multiple"
+                    control={<Radio />}
+                    label="Multiple options"
+                  />
+                  <FormControlLabel
+                    value="range"
+                    control={<Radio />}
+                    label="Range"
+                  />
+                  <FormControlLabel
+                    value="no-idea"
+                    control={<Radio />}
+                    label="No idea 🤷"
+                  />
+                </RadioGroup>
+              </FormControl> */}
+            </Stack>
+          </CardContent>
+        </Card>
+      </Container>
+    </SignedIn>
+  );
 }
