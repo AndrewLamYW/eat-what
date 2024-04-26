27c019c5d82544daca10afb0fc674b5d796cfb35
Andrew Lam <andrewlam.yw@gmail.com>
2024-04-26 08:18:52 +0800

    Rename page-authenticated.tsx to private.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Box from "@mui/material/Box";
-import PrivateHome from "./page-authenticated";
+import PrivateHome from "./private";
 import PublicHome from "./public";
 
 export default function Home() {
